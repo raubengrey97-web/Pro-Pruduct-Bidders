3e4f3b399dcd2e05a7d8aa59af28152ad761b107
raubengrey97-web <raubengrey97@gmail.com>
2026-06-06 12:45:36 +0300

    Create Home component for main page

---
+++ app/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main style={{ padding: 20 }}>
+      <h1>Pro Product Bidders</h1>
+      <p><a href="/signup">Sign Up</a> | <a href="/login">Login</a></p>
+    </main>
+  )
+}
